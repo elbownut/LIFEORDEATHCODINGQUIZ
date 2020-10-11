c4a8ba7b1c17fb52b7a66b59b4bd4dbe2bfed994
Edward Chan <ed.ph.chan@gmail.com>
2020-10-10 18:43:02 -0600

    added more code to js file

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ var a1;
 var a2;
 var a3;
 var a4;
+//Beginning layout.
+function quizopening() {
+    highscore.style.visibility = "hidden";
+
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "Start Quiz";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        question1();
+    });
+
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "View Scores";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        scorepage();
+    });
+}
+quizopening();
+
+//Timer.
+function countdown() {
+    timerInterval = setInterval(function() {
+    seconds --;
+    timer.textContent = "Time left: " + seconds;
+        if (seconds <= 0) {
+            clearInterval (timerInterval);
+            seconds = 0;
+            timer.textContent = "Time left: 0"
+            postquiz();
+        }
+    }, 1000);
+}

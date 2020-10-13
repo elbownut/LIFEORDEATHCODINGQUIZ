b04006fe9840954747e659f1e428c576eb4ead0b
Edward Chan <ed.ph.chan@gmail.com>
2020-10-12 18:08:56 -0600

    added more code

--- script.js
+++ script.js
@@ -47,3 +47,92 @@ function countdown() {
         }
     }, 1000);
 }
+//Adds remaining time to the score.
+function timepoints() {
+    score += seconds;
+}
+
+//Correct answer response.
+function correct() {
+    correctsound.play();
+}
+
+//Correct text display.
+function correctcw() {
+    var yesInterval = setInterval(function() {
+    cwtime --;
+    cw.setAttribute("style", "color: rgb(112, 177, 112)")
+    cw.textContent = "Correct!";
+        if (cwtime <= 0) {
+            clearInterval (yesInterval);
+            cw.textContent = "";
+            cwtime = 2;
+        }
+    }, 350);
+}
+
+//Wrong answer response.
+function wrong() {
+    wrongsound.play();
+    seconds -=15;
+}
+
+//Wrong text display.
+function wrongcw() {
+    var noInterval = setInterval(function() {
+    cwtime --;
+    cw.setAttribute("style", "color:rgb(185, 80, 80)");
+    cw.textContent = "Wrong!";
+        if (cwtime <= 0) {
+            clearInterval (noInterval);
+            cw.textContent = "";
+            cwtime = 2;
+        }
+    }, 350);
+}
+
+//Question 1 layout.
+function question1() {
+    a1.remove();
+    a2.remove();
+    countdown();
+    question.textContent = "Commonly used data types DO NOT include:";
+        
+    a1 = document.createElement("BUTTON");
+    a1.innerHTML = "String";
+    document.body.children[4].appendChild(a1);
+    a1.addEventListener("click", function () {
+        wrongcw();
+        question2();
+        wrong();
+    });
+
+    a2 = document.createElement("BUTTON");
+    a2.innerHTML = "Boolean";
+    document.body.children[4].appendChild(a2);
+    a2.addEventListener("click", function () {
+        wrongcw();
+        question2();
+        wrong();
+    });
+
+    a3 = document.createElement("BUTTON");
+    a3.innerHTML = "Number";
+    document.body.children[4].appendChild(a3);
+    a3.addEventListener("click", function () {
+        wrongcw();
+        question2();
+        wrong();
+    });
+
+    a4 = document.createElement("BUTTON");
+    a4.innerHTML = "Float";
+    document.body.children[4].appendChild(a4);
+    a4.addEventListener("click", function () {
+        correctcw();
+        question2();
+        correct();
+
+    });
+}
+
